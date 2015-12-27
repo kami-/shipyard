8c55ba7f5867bb755db3aa15cd4e924de75d21a3
kami_ <kamikaz333+c9@gmail.com>
2015-12-27 23:51:14 +0000

    Ignoring and removing TSD files.

--- src/ts/server/typings/tsd.d.ts
+++
@@ -1,8 +0,0 @@
-
-/// <reference path="express/express.d.ts" />
-/// <reference path="mime/mime.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="serve-static/serve-static.d.ts" />
-/// <reference path="body-parser/body-parser.d.ts" />
-/// <reference path="fs-extra/fs-extra.d.ts" />
-/// <reference path="lodash/lodash.d.ts" />
